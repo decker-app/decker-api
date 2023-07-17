808dc99166fbdfe0a73fa42c8e0a4d6c42f8a371
Goncharenko Nikita <goncharenkonikitae@gmail.com>
2023-07-17 21:14:54 +0600

    refactor: eliminate card service and controller

--- src/main/kotlin/ru/goncharenko/deck/controller/DeckController.kt
+++ src/main/kotlin/ru/goncharenko/deck/controller/DeckController.kt
@@ -1,26 +1,61 @@
 package ru.goncharenko.deck.controller
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import ru.goncharenko.deck.service.DeckService
+import java.time.Instant
 
 @RestController
-@RequestMapping("api/decks")
-class DeckController(
+@RequestMapping("api")
+class CardController(
     private val deckService: DeckService,
 ) {
-    @PostMapping
-    fun createDeck(@RequestBody dto: CreateDeckDTO) = deckService.createDeck(dto)
+    @PostMapping("decks")
+    fun createDeck(
+        @RequestBody dto: CreateDeckDTO
+    ) = deckService.createDeck(dto)
+
+    @PostMapping("decks/{deckId}/cards")
+    fun addCardToDeck(
+        @PathVariable deckId: String,
+        @RequestBody dto: AddCardDTO,
+    ) = deckService.addCard(deckId, dto)
+
+    @GetMapping("decks/{deckId}/cards/{cardId}")
+    fun getCardByDeckId(
+        @PathVariable deckId: String,
+        @PathVariable cardId: String,
+    ) = deckService.getCardFromDeck(deckId, cardId)
+
+    @GetMapping("decks/{deckId}/cards")
+    fun getCardsByDeckId(
+        @PathVariable deckId: String
+    ) = deckService.getCardsFromDeck(deckId)
 }
 
+data class AddCardDTO(
+    @JsonProperty("question")
+    val question: String,
+    @JsonProperty("answer")
+    val answer: String,
+)
+
+data class CardDTO(
+    @JsonProperty("question")
+    val question: String,
+    @JsonProperty("answer")
+    val answer: String,
+    @JsonProperty("bucket")
+    val bucket: Int,
+    @JsonProperty("lastViewDate")
+    val lastViewDate: Instant,
+    @JsonProperty("deckId")
+    val deckId: String,
+)
 
 data class CreateDeckDTO(
     @JsonProperty("name")
     val name: String,
-
     @JsonProperty("theme")
     val theme: String,
 )
--- src/main/kotlin/ru/goncharenko/deck/service/DeckService.kt
+++ src/main/kotlin/ru/goncharenko/deck/service/DeckService.kt
@@ -1,20 +1,81 @@
 package ru.goncharenko.deck.service
 
+import org.slf4j.LoggerFactory
 import org.springframework.data.mongodb.core.MongoOperations
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.data.mongodb.core.query.isEqualTo
 import org.springframework.stereotype.Service
+import ru.goncharenko.deck.collection.Card
 import ru.goncharenko.deck.collection.Deck
+import ru.goncharenko.deck.controller.AddCardDTO
 import ru.goncharenko.deck.controller.CreateDeckDTO
+import java.time.Instant
 
 
 @Service
 class DeckService(
     private val mongoOperations: MongoOperations,
 ) {
-    fun createDeck(dto: CreateDeckDTO) =
-        mongoOperations.save(
+    fun createDeck(dto: CreateDeckDTO): Deck {
+        logger.trace("Start saving Deck with name = ${dto.name} theme = ${dto.theme}")
+        val savedDeck = mongoOperations.save(
             Deck(
                 theme = dto.theme,
                 name = dto.name,
             )
         )
+        logger.info("Finish saving Deck with name = ${dto.name} theme = ${dto.theme}")
+        return savedDeck
+    }
+
+    fun addCard(deckId: String, dto: AddCardDTO): Card {
+        require(isDeckExist(deckId)) { "Deck with deckId=${deckId} not exist" }
+
+        return mongoOperations.save(
+            Card(
+                question = dto.question,
+                answer = dto.answer,
+                bucket = DEFAULT_BUCKET,
+                lastViewDate = DEFAULT_LAST_VIEW_DATE,
+                deckId = deckId
+            )
+        )
+    }
+
+    fun getCardsFromDeck(deckId: String): List<Card> {
+        require(isDeckExist(deckId)) { "Deck with deckId=${deckId} not exist" }
+
+        return mongoOperations.find(
+            Query(Card::deckId isEqualTo deckId),
+            Card::class.java
+        )
+    }
+
+
+    fun getCardFromDeck(deckId: String, cardId: String): Card? {
+        require(isDeckExist(deckId)) { "Deck with deckId=${deckId} not exist" }
+
+        return mongoOperations.findOne(
+            Query(
+                Criteria().andOperator(
+                    Card::deckId isEqualTo deckId,
+                    Card::cardId isEqualTo cardId,
+                )
+            ),
+            Card::class.java
+        )
+    }
+
+    private fun isDeckExist(deckId: String) = mongoOperations.exists(
+        Query(Deck::deckId isEqualTo deckId),
+        Deck::class.java
+    )
+
+    companion object {
+        const val DEFAULT_BUCKET = 1
+        val DEFAULT_LAST_VIEW_DATE = Instant.MIN!!
+
+        var logger = LoggerFactory.getLogger(DeckService::class.java)
+    }
 }
