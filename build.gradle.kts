1f13a424d24511943d77e0694a00459fade0c1fb
Goncharenko Nikita <goncharenkonikitae@gmail.com>
2023-08-11 23:15:43 +0300

    feat: add keycloak integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.security:spring-security-oauth2-resource-server")
+    implementation("org.springframework.security:spring-security-oauth2-jose")
+
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 
     implementation("org.springframework.boot:spring-boot-starter-validation")
